2d7da3ca379426b4ddcbd9bfc68f96d7c9f9262a
Viktor Jansson <viktorjansson@Viktors-MacBook-Air.local>
2017-01-01 22:16:29 +0100

    ready to push

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './data.service';
+import { FormsModule }   from '@angular/forms';
 
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-  <div>
-    <h1>Hello {{ greeting }}</h1>
-  </div>
-  `,
+  templateUrl: 'app.component.html',
   providers: [ DataService ]
 })
 export class AppComponent implements OnInit {
+  departures:any[] = [];
+  arrivals:any[] = [];
+  customTrip:any[] = [];
+  locations:any[] = [];
+  destination = {};
+  gullmars = '740021705';
+
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
-    this.dataService.fetchData();
+      this.dataService.getTripData(this.gullmars).subscribe(
+        (data) => {
+          for (let trip of data.Trip) {
+            this.departures.push(trip.LegList.Leg[0].Origin);
+            this.arrivals.push(trip.LegList.Leg[0].Destination);
+          }
+        });
   }
 
-  greeting = 'World!';
+  search(term:string){
+    this.dataService.searchLocation(term).subscribe(
+      locations => this.locations = locations.StopLocation);
+      if(this.locations){this.destination = this.locations[0];}
+  }
+
+  calcTrip(des:string){
+    this.dataService.getTripData(des).subscribe(
+      data => this.customTrip = data.Trip[0].LegList.Leg);
+  }
 
 }
--- app/data.service.ts
+++ app/data.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class DataService {
-  private dataUrl = 'https://api.resrobot.se/v2/trip?key=db32d1a8-1c10-4f95-b3d1-2cb5e2176152&originId=740024928&destId=740021705&format=json';  // URL to web api
+  private tripDataUrl = 'https://api.resrobot.se/v2/trip?key=db32d1a8-1c10-4f95-b3d1-2cb5e2176152&format=json&originId=740024928&destId=';
+  private locationDataUrl = 'https://api.resrobot.se/v2/location.name?key=db32d1a8-1c10-4f95-b3d1-2cb5e2176152&format=json&input=';
 
   constructor(private http: Http) { }
 
+  getTripData(destId: string){
+    return this.http.get(this.tripDataUrl + destId).map(
+      (res) => res.json()
+    );
+  }
 
-  fetchData(){
-    return this.http.get(this.dataUrl).subscribe(
-      (data) => console.log(data)
-    )
+  searchLocation(term: string){
+    return this.http.get(this.locationDataUrl + term).map(
+      (res) => res.json()
+    );
   }
 
 }
